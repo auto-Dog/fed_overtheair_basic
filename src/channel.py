ae5c93592795c3f5dc62a4bc8215a83c182bd33e
auto-Dog <2969860272@qq.com>
2024-03-01 13:59:44 +0800

    Further implement of OAC

--- src/channel.py
+++ src/channel.py
@@ -9,6 +9,8 @@ class wireless_channel():
         self.num_rx = args.num_rx
         self.args = args
         self.device = 'cuda' if args.gpu else 'cpu'
+        self.modulate_symbol = np.exp(1j*2*np.pi*2.4e9)   # 2.4GHz symbol, real part > 0
+        self.demodulate_symbol = np.exp(-1j*2*np.pi*2.4e9)   # 2.4GHz symbol
     
     def generate_channel_gain(self,num_tx,num_rx,device):
         '''
@@ -61,7 +63,7 @@ class wireless_channel():
                     csi_G += np.min(sigmai*sigmai)*np.matmul(ui,ui.T.conjugate())
                 F_mat,_,_ = np.linalg.svd(csi_G)
                 F_mat = F_mat[:,0].conjugate()
-                eta = max([np.abs(1/(F_mat.T @ h_coefficient[:,i].reshape(-1,1) @ h_coefficient[:,i].reshape(-1,1).T.conjugate() @ F_mat.conjugate())) for i in range(len(w))])/0.25
+                eta = max([np.abs(1/(F_mat.T @ h_coefficient[:,i].reshape(-1,1) @ h_coefficient[:,i].reshape(-1,1).T.conjugate() @ F_mat.conjugate())) for i in range(len(w))])/1
                 postcode_np = np.sqrt(eta)*F_mat
                 postcode = torch.from_numpy(postcode_np).to(device).flatten()
             # postcode = torch.ones(self.num_rx).to(device)
@@ -70,10 +72,9 @@ class wireless_channel():
                 for i in range(len(w)):  # Each cients' SIMO channel
                     # Put your precoding here:
                     if self.args.oac_method == 'none':
-                        precode = torch.ones(self.num_tx).to(device)/3
+                        precode = torch.ones(self.num_tx).to(device)
                     elif self.args.oac_method == 'naive':
-                        precode = rayleigh_coefficient[i,0].view(-1)/3
-                        print(precode)  # debug
+                        precode = rayleigh_coefficient[i,0].view(-1)
                     elif self.args.oac_method == 'mimo_oac':
                         precode = (h_coefficient[:,i].reshape(-1,1).T.conjugate() @ postcode_np.conjugate()) * \
                                 1/(postcode_np.T @ h_coefficient[:,i].reshape(-1,1) @ h_coefficient[:,i].reshape(-1,1).T.conjugate() @ postcode_np.conjugate())
@@ -88,17 +89,25 @@ class wireless_channel():
                         y_complex = rayleigh_coefficient[i,rx] * precode[0] * w[i][key] + self.generate_channel_noise(w[i][key],device,self.args.snr_db)
                         # 全部用precode[i,0]需要改进
                         y_post_complex += postcode[rx] * y_complex
+                    # 存疑，如何处理信号的符号
+                    sign_mask = torch.real(y_post_complex)>0
+                    sign_mask = sign_mask*1.0
+                    sign_mask[sign_mask==1] = 1.0
+                    sign_mask[sign_mask==0] = -1.0
                     if i==0:
-                        w_avg[key] = torch.real(y_post_complex)
+                        w_avg[key] = torch.abs(y_post_complex)*sign_mask
                     else:
-                        w_avg[key] += torch.real(y_post_complex)
-                # w_avg[key] = torch.div(w_avg[key], len(w))
+                        w_avg[key] += torch.abs(y_post_complex)*sign_mask
+                w_avg[key] = torch.div(w_avg[key], len(w))
 
         return w_avg
 
 if __name__ == '__main__':
     import argparse
     import torch.nn as nn
+    from utils import average_weights
+    from tqdm import tqdm
+    
     parser = argparse.ArgumentParser()
     # federated arguments (Notation for the arguments followed from paper)
     parser.add_argument('--num_tx', type=int, default=1)
@@ -115,9 +124,23 @@ if __name__ == '__main__':
             val.append(mseloss(w1[key],w2[key]).numpy())
         return np.mean(np.array(val))
 
+    # 简单测试
     channel = wireless_channel(args)
     model = {'layer1':torch.Tensor([1,2,-1]).to('cpu')}
-    model_list = [model,model,model]
+    model_list = [model,model,model,model]
     print(channel.channel_process(model_list))
     print('Model mse:',eval_model_mse(model,channel.channel_process(model_list)))
-    
+
+    # result_list = []
+    # for iter in tqdm(range(10)):
+    #     channel = wireless_channel(args)
+    #     model_list = []
+    #     for client in range(10):
+    #         model = {'layer1':torch.Tensor([1,2,-1]).to('cpu'),'layer2':torch.empty(20,10).uniform_(-10,10).to('cpu')}
+    #         model_list.append(model)
+    #     model_target = average_weights(model_list)
+    #     # print(channel.channel_process(model_list))
+    #     # print('Model mse:',eval_model_mse(model,channel.channel_process(model_list)))
+    #     result_list.append(eval_model_mse(model_target,channel.channel_process(model_list)))
+    # print(result_list)
+    # print('Avg MSE:',np.mean(np.array(result_list)))
